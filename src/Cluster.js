a8adde8e2f1e12e46b68a6e4106b5cebd4be5eed
Yusuke Ise <129273697+ise-bachelor@users.noreply.github.com>
2024-07-22 17:51:11 +0900

    文字が付箋からはみ出さないようにした

--- src/Cluster.js
+++ src/Cluster.js
@@ -1,7 +1,7 @@
 import CreateSN from './StickyNote';
 
 function DisplayCluster(){
-  const cluster = [{id: 0, txt: 'aaa', x: 10, y: 10},{id: 1, txt: 'bbb', x: 70, y: 10},{id: 2, txt: 'ccc', x: 10, y: 70},{id: 3, txt: 'ddd', x: 70, y: 70}];
+  const cluster = [{id: 0, txt: 'aaa', x: 10, y: 10},{id: 1, txt: 'bbb', x: 70, y: 10},{id: 2, txt: 'ccc', x: 10, y: 70},{id: 3, txt: 'ddddddddd', x: 70, y: 70}];
   return(
     <>
     {
