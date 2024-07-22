f230c22e6b189f1e35d20fd2c38815b230be0108
Yusuke Ise <129273697+ise-bachelor@users.noreply.github.com>
2024-07-22 17:40:04 +0900

    付箋を作成

--- src/Cluster.js
+++ src/Cluster.js
@@ -1,11 +1,18 @@
 import CreateSN from './StickyNote';
 
 function DisplayCluster(){
-  const cluster = [];
+  const cluster = [{id: 0, txt: 'aaa', x: 10, y: 10},{id: 1, txt: 'bbb', x: 70, y: 10},{id: 2, txt: 'ccc', x: 10, y: 70},{id: 3, txt: 'ddd', x: 70, y: 70}];
   return(
     <>
-    
-    <CreateSN txt={'aaa'}/>
+    {
+      (function () {
+        const list = [];
+        cluster.forEach((sn) => {
+          list.push(<CreateSN sn={sn} key={sn.id}/>);
+        });
+        return list;
+      }())
+    }
     </>
   );
 }
--- src/StickyNote.js
+++ src/StickyNote.js
@@ -1,7 +1,8 @@
-function CreateSN({txt}){
+function CreateSN({sn}){
+  console.log(sn);
   return(
-    <div className="SN">
-      <p>{txt}</p>
+    <div className="SN" style={{zIndex: sn.id, position: "absolute", top: `${sn.x}px`, left: `${sn.y}px`}}>
+      <p>{sn.txt}</p>
     </div>
   );
 }
